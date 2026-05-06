301c2fbb8aad04227fe93d1aa88d10f9ce1860db
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-06 08:17:40 +0200

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,1 +1,12 @@
+class User:
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
 
+    def login(self):
+        return f"{self.name} logged in"
+
+    def register(self):
+        return f"{self.name} registered"

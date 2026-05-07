797800be268ac122c2a9fac747197c076fdca867
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-07 17:08:29 +0200

    Update prototype.py

--- creational_patterns/prototype.py
+++ creational_patterns/prototype.py
@@ -1,8 +1,8 @@
 import copy
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+class AssignmentTemplate:
+    def __init__(self, title):
+        self.title = title
 
     def clone(self):
         return copy.deepcopy(self)

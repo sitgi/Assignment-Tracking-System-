57c12f1104c51e9c3fbbb9c6ae96b9072a19e559
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-07 16:55:25 +0200

    Update simple_factory.py

--- creational_patterns/simple_factory.py
+++ creational_patterns/simple_factory.py
@@ -1,1 +1,17 @@
+class Car:
+    def drive(self):
+        return "Driving Car"
 
+class Bike:
+    def drive(self):
+        return "Riding Bike"
+
+class VehicleFactory:
+    @staticmethod
+    def create_vehicle(type):
+        if type == "car":
+            return Car()
+        elif type == "bike":
+            return Bike()
+        else:
+            raise ValueError("Invalid vehicle type")

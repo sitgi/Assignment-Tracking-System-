5441d08da1fc9af5e3584057d3aedeeac6366055
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-07 16:53:18 +0200

    Create reminder.py

---
+++ src/reminder.py
@@ -0,0 +1,9 @@
+class Reminder:
+    def __init__(self, time):
+        self.time = time
+
+    def schedule(self):
+        return "Reminder scheduled"
+
+    def send(self):
+        return "Reminder sent"

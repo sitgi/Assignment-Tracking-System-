7c0ed511035a01ad18d348039ef9af6c42bd8815
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-07 17:00:39 +0200

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,1 +1,6 @@
 
+from creational_patterns.simple_factory import VehicleFactory
+
+def test_vehicle_factory():
+    car = VehicleFactory.create_vehicle("car")
+    assert car.drive() == "Driving Car"

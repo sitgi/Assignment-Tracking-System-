80b1cb664baffa43415e1e0efb25c16a8b35ca74
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-12 17:44:12 +0200

    Create database_assignment_repository.py

---
+++ repositories/database_assignment_repository.py
@@ -0,0 +1,15 @@
+from repositories.assignment_repository import AssignmentRepository
+
+class DatabaseAssignmentRepository(AssignmentRepository):
+
+    def save(self, assignment):
+        pass
+
+    def find_by_id(self, id):
+        pass
+
+    def find_all(self):
+        pass
+
+    def delete(self, id):
+        pass

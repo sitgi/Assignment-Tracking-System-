932e4ec946b0c27dd775909f085138256d94daa3
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-20 20:39:07 +0200

    Update test_user_service.py

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -1,1 +1,24 @@
 
+from services.user_service import UserService
+
+def test_create_user():
+
+    service = UserService()
+
+    user = service.create_user(
+        "John",
+        "john@email.com"
+    )
+
+    assert user["name"] == "John"
+
+
+def test_invalid_user():
+
+    service = UserService()
+
+    try:
+        service.create_user("", "")
+        assert False
+    except ValueError:
+        assert True

125105419d2665a6a98c51159aa6edbb24d7db48
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-20 20:35:53 +0200

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -1,1 +1,23 @@
 
+from repositories.inmemory.inmemory_assignment_repository import InMemoryAssignmentRepository
+
+class UserService:
+
+    def __init__(self):
+        self.users = []
+
+    def create_user(self, name, email):
+
+        if not name or not email:
+            raise ValueError("Name and email required")
+
+        user = {
+            "name": name,
+            "email": email
+        }
+
+        self.users.append(user)
+        return user
+
+    def get_users(self):
+        return self.users

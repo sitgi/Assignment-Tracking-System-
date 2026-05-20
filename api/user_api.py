2b7910f597d46ba54a6a18124bde257d89074041
Okuhle Sithole <221137610@mycput.ac.za>
2026-05-20 20:46:19 +0200

    Update user_api.py

--- api/user_api.py
+++ api/user_api.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from services.user_service import UserService
 
+app = FastAPI()
+
+user_service = UserService()
+
+@app.get("/api/users")
+def get_users():
+    return user_service.get_users()
+
+@app.post("/api/users")
+def create_user(name: str, email: str):
+    return user_service.create_user(
+        name,
+        email
+    )
